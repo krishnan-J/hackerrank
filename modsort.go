95ab88d08efa11ca5679459995cf1b8f644a6cbf
krishnan <115535051+krishnan-j@users.noreply.github.com>
2022-11-09 19:25:03 +0530

    Add files via upload

---
+++ modsort.go
@@ -0,0 +1,19 @@
+// You can edit this code!
+// Click here and start typing.
+package modsort
+
+import (
+	"sort"
+)
+
+func ModuloSort(arr []string) (res []string) {
+	a := [3][]string{}
+	for _, v := range arr {
+		a[len(v)%3] = append(a[len(v)%3], v)
+	}
+	for i := range a {
+		sort.Strings(a[i])
+		res = append(res, a[i]...)
+	}
+	return
+}
